575ed7de579336170c737f0a324d40d1f03378fa
Supersaver01 <128857553+Supersaver01@users.noreply.github.com>
2023-11-25 09:17:27 -0600

    new changes with force unlock

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "aws_ubuntu" {
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.ubuntu.id
   user_data              = file("userdata.tpl")
-  vpc_security_group_ids = ["${aws_security_group.ddemo_sg_pull-terraform-class.id}"]
+  vpc_security_group_ids = ["${aws_security_group.demo_sg_pull-terraform-class.id}"]
   key_name               = "EC2Instance"
 
     tags = {
